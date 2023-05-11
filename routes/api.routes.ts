8a07a0ad6eff997e8a309c72bdc11b5c72a5939b
stoneheart428 <lovely4you2ai8ni@gmail.com>
2023-05-11 07:59:45 -0700

    fix: update game

--- routes/api.routes.ts
+++ routes/api.routes.ts
@@ -1,7 +1,8 @@
 import { Request, Response, Router } from "express";
 import * as auth from "../controllers/auth.controller";
-import * as challenge from "../controllers/challenges.controller";
-import * as wallet from "../controllers/wallet.controller";
+import * as challenge from '../controllers/challenges.controller';
+import * as wallet from '../controllers/wallet.controller';
+import * as game from '../controllers/game.controller';
 // import Authenticate from "../service/auth";
 
 /**
@@ -12,22 +13,27 @@ const router = Router();
 
 // test
 router.get("/test", (req: Request, res: Response) => {
-  res.json({ status: "success" });
-});
+    res.json({status: "success"})
+})
 
 // Authentication
 router.post("/signin", auth.SignIn);
 router.post("/signup", auth.SignUp);
 
 // Administrator challenges
-router.get("/challenge/index", challenge.index);
-router.post("/challenge/save", challenge.save);
-router.delete("/challenge/remove/:id", challenge.remove);
+router.get('/challenge/index', challenge.index);
+router.post('/challenge/save', challenge.save);
+router.delete('/challenge/remove/:id', challenge.remove);
 
 // Wallet
-router.post("/deposit", wallet.deposit);
-router.post("/withdraw", wallet.withdraw);
-router.post("/getUserInfo", wallet.getUserInfo);
-router.post("/swap", wallet.swap);
-router.post("/withdrawHistory", wallet.withdrawHistory);
+router.post('/withdraw/index', wallet.withdraw_index);
+router.post('/deposit', wallet.deposit);
+router.post('/withdraw', wallet.withdraw);
+
+
+
+
+// Game
+router.post('/game/start', game.start);
+router.post('/get-challenge', game.get_challenge);
 export default router;
--- service/interfaces.ts
+++ service/interfaces.ts
@@ -5,29 +5,23 @@ import { Document } from "mongoose";
  * Document class inheritance
  */
 export interface IUser extends Document {
-  username: string;
-  email: string;
-  password: string;
-  firstname: string;
-  lastname: string;
-  money: {
-    usdt: number;
-    busd: number;
-    quest: number;
-    bitp: number;
-    usd: number;
-    cake: number;
-  };
-  address: {
-    ether: { privateKey: string; address: string };
-    bitcoin: { privateKey: string; address: string };
-    tron: { privateKey: string; address: string };
-  };
-  txcount: {
-    busd: number;
-    usdt: number;
-    cake: number;
-  };
+    username: string;
+    email: string;
+    password: string;
+    firstname: string;
+    lastname: string;
+    money: {
+        usdt: number;
+        busd: number;
+        quest: number;
+        bitp: number;
+        usd: number;
+    };
+    address: {
+        ether: { privateKey: string, address: string },
+        bitcoin: { privateKey: string, address: string },
+        tron: { privateKey: string, address: string }
+    }
 }
 
 /**
@@ -35,52 +29,75 @@ export interface IUser extends Document {
  * Document class inheritance
  */
 export interface IChallenge extends Document {
-  title: string;
-  difficalty: number;
-  streak: number;
-  amount: number;
-  qc: number;
-  coin_sku: string;
-  status: number;
+    title: string;
+    difficalty: number;
+    description: string;
+    streak: number;
+    amount: number;
+    qc: number;
+    coin_sku: string;
+    loss_back: string;
+    status: number;
+}
+
+
+export interface IPlayChallenge extends Document {
+    user_id: string;
+    challenge_id: string;
+    current_match: string;
+    win_match: string;
+    loss_match: string;
+    tot_match: string;
+    won_challenge: string;
+    status: string;
+}
+
+export interface IPlayedChallenge extends Document {
+    user_id: string;
+    challenge_id: string;
+    start_match: string;
+    end_match: string;
+    winorloss: string;
+    status: number;
 }
 
 export interface IHistory extends Document {
-  user: string;
-  coin: string;
-  amount: number;
-  address: string;
+    user: string,
+    coin: string,
+    amount: number,
+    address: string
 }
 
 export interface ITransaction extends Document {
-  coin: string;
-  amount: number;
-  address: string;
-  user: string;
+    coin: number;
+    amount: number;
+    address: string;
+    user: string;
 }
 
 interface IResponse {
-  [key: string]: any;
+    [key: string]: any;
 }
 
 interface IWalletResponse {
-  address: string | unknown;
-  privateKey: string;
-  publicKey?: string;
-  mnemonic?: string;
-  nonce?: number;
-  seed?: string;
+    address: string | unknown;
+    privateKey: string;
+    publicKey?: string;
+    mnemonic?: string;
+    nonce?: number;
+    seed?: string;
 }
 
 type IBalanceResponse = number | string;
 
 export const response = (args: IResponse) => {
-  return args;
-};
+    return args;
+}
 
 export const walletResponse = (args: IWalletResponse) => {
-  return args;
-};
+    return args;
+}
 
 export const balanceResponse = (arg: IBalanceResponse) => {
-  return arg;
-};
+    return arg;
+}
