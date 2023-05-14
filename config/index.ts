b0f44bb2f12ad90e4c23d4749f3d3edf774a0192
crazy0629 <webkiller0104@gmail.com>
2023-05-13 21:12:49 -0700

    change server url to my test url

--- config/index.ts
+++ config/index.ts
@@ -7,7 +7,7 @@ export const MONGO_URI = process.env.MONGO_URI as string;
 ("mongodb+srv://kuzma:mr.doctor0104@cluster0.danmkpq.mongodb.net/bitsport?retryWrites=true&w=majority");
 export const SECRET_KEY =
   (process.env.SECRET_KEY as string) || "bitsport-bitpool-node-project";
-export const SERVER_URI = "http://54.176.177.230";
+export const SERVER_URI = "http://54.241.67.65";
 export const SERVER_PORT = (process.env.SERVER_PORT as string) || 8000;
 
 // Email Verification
